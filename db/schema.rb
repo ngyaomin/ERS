81c8caf625ab05df039c0873c44fe3771e9fd160
Unknown <ngyaomin@ymail.com>
2017-02-17 13:36:42 +0800

    Add user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217053256) do
+ActiveRecord::Schema.define(version: 20170217053520) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170217053256) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
