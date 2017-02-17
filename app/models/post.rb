964e45c0d6e238a54d37bbd2bae79a280cc705d5
Unknown <ngyaomin@ymail.com>
2017-02-17 18:11:00 +0800

    Debug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :comments, dependent: :destroy
   validates :subject, presence: true
   validates :content, presence: true
